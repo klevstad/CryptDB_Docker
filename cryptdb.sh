3f663e9376fc0c7d3dda39e1d497d344c7a388e1
agribu <agribu@users.noreply.github.com>
2017-10-28 15:13:39 +0200

    small bug fix
    enter working directory first, otherwise queries on database via cryptdb proxy are failing

--- cryptdb.sh
+++ cryptdb.sh
@@ -6,6 +6,7 @@ export EDBDIR=/opt/cryptdb
 case "$1" in
   start)
     echo "Starting cryptdb"
+    cd $EDBDIR
     "$EDBDIR"/bins/proxy-bin/bin/mysql-proxy --plugins=proxy --event-threads=4 --max-open-files=1024 --proxy-lua-script=$EDBDIR/mysqlproxy/wrapper.lua --proxy-address=0.0.0.0:3307 --proxy-backend-addresses=127.0.0.1:3306
     ;;
   stop)
